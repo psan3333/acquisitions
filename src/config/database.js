3fbce7f20ea50bcff942ce7c9b1d1610939ff915
psan3333 <psan3333@mail.ru>
2025-12-04 12:23:28 +0700

    Set up local postgres w/ drizzle

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+const sql = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const db = drizzle(sql);
+
+export { db, sql };
